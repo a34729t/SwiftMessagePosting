878d628d2bd0a2c0baeb731bbfd56d8a19ec89ca
Nicolas Flacco <nico@Nicolass-MacBook-Pro.local>
2014-09-21 13:50:25 -0700

    Custom interface builder elements. Extremely unstable and painful to use. Thanks Apple!

---
+++ MessagePosting/NibDesignable.swift
@@ -0,0 +1,92 @@
+//
+//  NibDesignable.swift
+//
+//  Copyright (c) 2014 Morten Bøgh
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import UIKit
+import Foundation
+
+@IBDesignable
+public class NibDesignable: UIView {
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupNib()
+    }
+    
+    // MARK: - NSCoding
+    required public init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupNib()
+    }
+    
+    // MARK: - Nib loading
+    
+    /**
+    Called in init(frame:) and init(aDecoder:) to load the nib and add it as a subview.
+    */
+    private func setupNib() {
+        var view = self.loadNib()
+        view.frame = self.bounds
+        view.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        self.addSubview(view)
+    }
+    
+    /**
+    Called to load the nib in setupNib().
+    
+    :returns: UIView instance loaded from a nib file.
+    */
+    public func loadNib() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: self.nibName(), bundle: bundle)
+        return nib.instantiateWithOwner(self, options: nil)[0] as UIView
+    }
+    
+    /**
+    Called in the default implementation of loadNib(). Default is class name.
+    
+    :returns: Name of a single view nib file.
+    */
+    public func nibName() -> String {
+        return self.dynamicType.description().componentsSeparatedByString(".").last!
+    }
+    
+
+    public func liveDebugLog(message: String) {
+        #if !(TARGET_OS_IPHONE)
+            let logPath = "/tmp/XcodeLiveRendering.log"
+            if !NSFileManager.defaultManager().fileExistsAtPath(logPath) {
+                NSFileManager.defaultManager().createFileAtPath(logPath, contents: NSData(), attributes: nil)
+            }
+            
+            var fileHandle = NSFileHandle(forWritingAtPath: logPath)
+            fileHandle.seekToEndOfFile()
+            
+            let date = NSDate()
+            let bundle = NSBundle(forClass: self.dynamicType)
+            let application: AnyObject? = bundle.objectForInfoDictionaryKey("CFBundleName")
+            let data = "\(date) \(application) \(message)\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+            fileHandle.writeData(data!)
+        #endif
+    }
+}
